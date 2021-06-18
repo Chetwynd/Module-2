da5cec0e8c9d5a83a4c6e02c1c1e40c51374d5ad
Wilfred Khalik <wilfredkhalik@gmail.com>
2021-06-18 22:41:41 +0000

    Main code body complete

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-function renderInventory(inventory)
+function renderInventory(inventory) 
 {
-  //iterate through inventory to access each item
-  for(var index = 0; index < inventory.length; index++)
-  {
-    var currentObject = inventory[index];
-    var currentShoeMaker = currentObject['name'];
-    var currentShoeDetailsArray = currentObject['shoes'];
-    var currentListArray = [];
-    // first add the current shoemaker to our list array
-    currentListArray.push(currentShoeMaker);
-    // then iterate through the currentShoeNameArray to add the shoes and prices
-    for(var shoeIndex = 0; shoeIndex < currentShoeNameArray.length; shoeIndex++)
-    {
-      var currentShoeName = currentShoeDetailsArray[shoeIndex]['name'];
-      var currentShoePrice = currentShoeDetailsArray[shoeIndex]['price'];
-      
-      currentListArray.push(currentShoeName, currentShoePrice);
-    }
-  }
+ var flatList = '';
+ 
+ //access itens in the object and create a list
+ for(var index = 0; index < inventory.length; index++)
+ {
+   var currentDesignerName = inventory[index]['name'];
+   var currentObjectsShoeArray = inventory[index]['shoes'];
+   
+   // access items in the shoe array
+   for(var innerIndex = 0; innerIndex < currentObjectsShoeArray.length; innerIndex++)
+   {
+     var currentShoeName = currentObjectsShoeArray[innerIndex]['name'];
+     var currentShoePrice = currentObjectsShoeArray[innerIndex]['price'];
+
+     if(index === inventory.length - 1 && innerIndex === currentObjectsShoeArray.length - 1)
+     {
+      flatList += currentDesignerName + ', ' + currentShoeName + ', ' + currentShoePrice;
+      continue;
+     }
+     else
+     {
+      flatList += currentDesignerName + ', ' + currentShoeName + ', ' + currentShoePrice + '\n';
+     }
+     
+   }
+ }
+ console.log(flatList);
 }
+
+var currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+];
+
+renderInventory(currentInventory);
