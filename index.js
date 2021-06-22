61640278bc56ae0685a6e3963a78175732c98b08
Wilfred Khalik <wilfredkhalik@gmail.com>
2021-06-22 01:26:01 +0000

    pasted code

--- index.js
+++ index.js
@@ -1,61 +1,23 @@
-function findLacedWords(Inventory)
-{// accumulator
-  var resultArray = [];
-  
-  // iterate through the inventory to look at each shoe name
-  for(var index = 0; index < Inventory.length; index++)
-  {
-    var currentShoeObject = Inventory[index]['shoes'];
-    // iterate through every shoe type in the shoe object
-    for(var shoeNameIndex = 0; shoeNameIndex < currentShoeObject.length; shoeNameIndex++)
-    {
-      var currentShoe = currentShoeObject[index]['name'];
-      
-      if(currentShoe.includes('lace'))
-      {
-       var currentShoeToArray = currentShoe.split('');
-       var newObject = {};
-      
-       newObject['nameWords'] = currentShoeToArray;
-      
-       // iterate through the currentShoeToArray to find a match for the word 'lace'
-       for(var shoeToArrayIndex = 0; shoeToArrayIndex < currentShoeToArray.length; shoeToArrayIndex++)
-       {
-         var currentShoeArrayItem = currentShoeToArray[shoeToArrayIndex];
-         var currentIndex = shoeToArrayIndex;
-        
-         var currentShoeArrayItem = currentShoeToArray[shoeToArrayIndex];
-         if(currentArrayItem.includes('lace'))
-         {
-           newObject['targetWordIndex'] = currentIndex;
-         }
-       }
-       resultArray.push(newObject);
-      }// end of if statement
-     }
-  }
-  return resultArray;
-}
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
 
-var currentInventory = [
-  {
-    name: 'Brunello Cucinelli',
-    shoes: [
-      {name: 'tasselled black low-top lace-up', price: 1000},
-      {name: 'tasselled green low-top lace-up', price: 1100},
-      {name: 'plain beige suede moccasin', price: 950},
-      {name: 'plain olive suede moccasin', price: 1050}
-    ]
-  },
-  {
-    name: 'Gucci',
-    shoes: [
-      {name: 'red leather laced sneakers', price: 800},
-      {name: 'black leather laced sneakers', price: 900}
-    ]
-  }
-];
+function duplicateZeros(arr)
+{
+ // iterate through the array
+ for(var index = 0; index < arr.length; index++)
+ {
+   var currentItem = arr[index];
 
-var expected = findLacedWords(currentInventory);
+   if(currentItem === 0)
+   {
+     arr.splice(index, 0, 0);
+   }
+ }
+ console.log(arr);
+};
 
-console.log(expected);
+var array = [1,0,2,3,0,4,5,0];
+
+duplicateZeros(array);
